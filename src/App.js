61d21947d5467c89b56e7ec768a51b01ab5b8efc
Alex <alexsalazarny@gmail.com>
2022-09-07 20:37:22 -0400

    Added nav items implementing nav bar next

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
-  return <h1>my app</h1>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/" element={<Offers />} />
+          <Route path="/" element={<Profile />} />
+          <Route path="/" element={<Signin />} />
+          <Route path="/" element={<SignUp />} />
+          <Route path="/" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
